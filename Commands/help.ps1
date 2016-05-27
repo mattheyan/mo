1644e18f2086cd3af969f4b74567f9b47bcf5305
Bryan Matthews <mattheyan@gmail.com>
2016-05-27 15:28:19 -0400

    Add general help listing.

--- Commands/help.ps1
+++ Commands/help.ps1
@@ -19,27 +19,47 @@ param(
 
 $here = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-$commandPath = Join-Path $here "$($Command).ps1"
+if ($Command) {
+    $commandPath = Join-Path $here "$($Command).ps1"
+    if (Test-Path $commandPath) {
+        $help = Get-Help $commandPath
+        $usageText = ((($help.syntax | Out-String) -split "`r`n") | Where-Object -Property Length -GT 0) -join ' '
+        $usage = $usageText.Replace($commandPath, $Command).Replace("  [<CommonParameters>]", '').Replace(" [<CommonParameters>]", '').Replace(" [[-Params] <Object>]", '').Replace(" [[-Params]  <Object>]", '')
+        $parameters = $help.parameters | ForEach-Object parameter | Where-Object -Property name -NE 'Params'
 
-if (Test-Path $commandPath) {
-    $help = Get-Help $commandPath
-    $usageText = ((($help.syntax | Out-String) -split "`r`n") | Where-Object -Property Length -GT 0) -join ' '
-    $usage = $usageText.Replace($commandPath, $Command).Replace("  [<CommonParameters>]", '').Replace(" [<CommonParameters>]", '').Replace(" [[-Params] <Object>]", '').Replace(" [[-Params]  <Object>]", '')
-    $parameters = $help.parameters | ForEach-Object parameter | Where-Object -Property name -NE 'Params'
-
-    Write-Host "Usage: mo $usage"
+        Write-Host "Usage: mo $usage"
+        Write-Host ""
+        Write-Host $help.Synopsis
+        Write-Host ""
+        $parameters | ForEach-Object {
+            Write-Host " -$($_.name)" -NoNewLine
+            for ($len = $_.name.Length + 2; $len -lt 20; $len += 1) {
+                Write-Host ' ' -NoNewLine
+            }
+            $_.description | ForEach-Object Text | Write-Host
+        } | Out-Null
+        Write-Host ""
+    } else {
+        Write-Host "Unknown command: '$($Command)'" -ForegroundColor Red
+        exit 1
+    }
+} else {
+    $moRoot = (Split-Path (Split-Path (gcm mo).Path -Parent) -Parent)
+    $moVersion = Get-Content (Join-Path $moRoot "bin\version.txt")
+    Write-Host "Mo v$($moVersion)"
+    Write-Host "Usage: Mo <command> [args] [options]"
+    Write-Host "Type 'Mo help <command>' for help on a specific command."
     Write-Host ""
-    Write-Host $help.Synopsis
+    Write-Host "Available commands:"
     Write-Host ""
-    $parameters | ForEach-Object {
-        Write-Host " -$($_.name)" -NoNewLine
-        for ($len = $_.name.Length + 2; $len -lt 20; $len += 1) {
+    Get-ChildItem $here | ForEach-Object {
+        $help = Get-Help $_.FullName
+        $name = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+        Write-Host " $($name)" -NoNewLine
+        for ($len = $name.Length + 1; $len -lt 20; $len += 1) {
             Write-Host ' ' -NoNewLine
         }
-        $_.description | ForEach-Object Text | Write-Host
-    } | Out-Null
+        Write-Host $help.Synopsis
+    }
     Write-Host ""
-} else {
-    Write-Host "Unknown command: '$($Command)'" -ForegroundColor Red
-    exit 1
 }
