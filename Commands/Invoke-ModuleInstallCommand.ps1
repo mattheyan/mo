a83d8941933b8b6883b5f8604c304f6012c01d5f
Bryan Matthews <mattheyan@gmail.com>
2017-07-22 16:26:41 -0400

    Implement global install

--- Commands/Invoke-ModuleInstallCommand.ps1
+++ Commands/Invoke-ModuleInstallCommand.ps1
@@ -5,122 +5,162 @@ Installs a module.
 
 #>
 function Invoke-ModuleInstallCommand {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='LocalModule')]
     param(
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(ParameterSetName='LocalModule', Position=0)]
+        [Parameter(ParameterSetName='GlobalModule', Mandatory=$true, Position=0)]
         [string]$Name,
 
-        [string]$Version
+        [string]$Version,
+
+        [string]$Source,
+
+        [Parameter(ParameterSetName='GlobalModule', Mandatory=$true)]
+        [switch]$Global,
+
+        [ValidateSet('AllUsers', 'CurrentUser')]
+        [Parameter(ParameterSetName='GlobalModule', Mandatory=$true)]
+        [string]$Scope
     )
 
-    Write-Verbose "Searching for module root from '$($PWD.Path)'..."
-
-    $root = Find-ModuleRoot
-
-    if ($root) {
-        Write-Verbose "Found module root '$($root)'."
-
-        $findPackageArgs = @{
+    if ($Global.IsPresent) {
+        $findModuleArgs = @{
             'Name' = $Name
-            'Provider' = 'NuGet'
         }
 
         if ($Version) {
-            $findPackageArgs['RequiredVersion'] = $Version
+            $findModuleArgs['RequiredVersion'] = $Version
         }
 
-        Write-Message "Searching for package '$($Name)'..."
-        $package = Find-Package @findPackageArgs
+        if ($Source) {
+            $findModuleArgs['Repository'] = $Source
+        }
 
-        if ($package) {
-            $tempFolder = Get-TempFolder
+        Write-Message "Searching for module '$($Name)'..."
+        $module = Find-Module @findModuleArgs
 
-            $savePackageArgs = @{
-                'Path' = $tempFolder
+        if ($module) {
+            $installModuleArgs = @{}
+
+            if ($Scope) {
+                $installModuleArgs['Scope'] = $Scope
+            }
+
+            Write-Message "Installing module $($module.Name)@$($module.Version)..."
+            $module | Install-Module @installModuleArgs
+        } else {
+            Write-Error "Couldn't find module '$($Name)'."
+        }
+    } else {
+        Write-Verbose "Searching for module root from '$($PWD.Path)'..."
+
+        $root = Find-ModuleRoot
+
+        if ($root) {
+            Write-Verbose "Found module root '$($root)'."
+
+            $findPackageArgs = @{
+                'Name' = $Name
                 'Provider' = 'NuGet'
             }
 
             if ($Version) {
-                $savePackageArgs['RequiredVersion'] = $Version
+                $findPackageArgs['RequiredVersion'] = $Version
             }
 
-            Write-Message "Downloading package $($package.Name)@$($package.Version)..."
-            $packages = $package | Save-Package @savePackageArgs
+            Write-Message "Searching for package '$($Name)'..."
+            $package = Find-Package @findPackageArgs
 
-            $packagesToUnpack = [array]($packages | Where-Object {
-                Write-Message "Downloaded package $($_.PackageFilename)."
+            if ($package) {
+                $tempFolder = Get-TempFolder
 
-                if (Test-Path "$($root)\Modules\$($_.Name)") {
-                    if (Test-Path "$($root)\Modules\$($_.Name)\$($_.Version)") {
-                        Write-Message "Package $($_.Name)@$($_.Version) is already installed."
-                        return $false
-                    }
+                $savePackageArgs = @{
+                    'Path' = $tempFolder
+                    'Provider' = 'NuGet'
+                }
 
-                    if (Test-Path "$($root)\Modules\$($_.Name)\$($_.Name).psd1") {
-                        try {
-                            $manifest = Import-PSData "$($root)\Modules\$($_.Name)\$($_.Name).psd1"
-                            if ($manifest.ModuleVersion -ge $_.Version) {
-                                Write-Message "Package $($_.Name)@$($_.Version) is already installed."
-                                return $false
+                if ($Version) {
+                    $savePackageArgs['RequiredVersion'] = $Version
+                }
+
+                Write-Message "Downloading package $($package.Name)@$($package.Version)..."
+                $packages = $package | Save-Package @savePackageArgs
+
+                $packagesToUnpack = [array]($packages | Where-Object {
+                    Write-Message "Downloaded package $($_.PackageFilename)."
+
+                    if (Test-Path "$($root)\Modules\$($_.Name)") {
+                        if (Test-Path "$($root)\Modules\$($_.Name)\$($_.Version)") {
+                            Write-Message "Package $($_.Name)@$($_.Version) is already installed."
+                            return $false
+                        }
+
+                        if (Test-Path "$($root)\Modules\$($_.Name)\$($_.Name).psd1") {
+                            try {
+                                $manifest = Import-PSData "$($root)\Modules\$($_.Name)\$($_.Name).psd1"
+                                if ($manifest.ModuleVersion -ge $_.Version) {
+                                    Write-Message "Package $($_.Name)@$($_.Version) is already installed."
+                                    return $false
+                                }
+                            } catch {
+                                Write-Warning "Unable to parse module manifest '.\Modules\$($_.Name)\$($_.Name).psd1"
                             }
-                        } catch {
-                            Write-Warning "Unable to parse module manifest '.\Modules\$($_.Name)\$($_.Name).psd1"
                         }
                     }
-                }
 
-                return $true
-            })
+                    return $true
+                })
 
-            if ($packagesToUnpack.Count -gt 0) {
-                $modulesFolder = "$($root)\Modules"
+                if ($packagesToUnpack.Count -gt 0) {
+                    $modulesFolder = "$($root)\Modules"
 
-                $modulesFolderHash = $modulesFolder.ToLower() | Get-Hash -OutputType 'Bytes'
+                    $modulesFolderHash = $modulesFolder.ToLower() | Get-Hash -OutputType 'Bytes'
 
-                $modulesFolderTempName = [Convert]::ToBase64String($modulesFolderHash)
+                    $modulesFolderTempName = [Convert]::ToBase64String($modulesFolderHash)
 
-                if (Test-Path "$tempFolder\.staging\$modulesFolderTempName") {
-                    Write-Verbose "Cleaning up old temp directory..."
-                    Remove-Item "$tempFolder\.staging\$modulesFolderTempName" -Force -Recurse -Confirm:$false | Out-Null
-                }
-
-                mkdir "$tempFolder\.staging\$modulesFolderTempName" | Out-Null
-
-                $packagesToUnpack | ForEach-Object {
-                    $nupkgFile = "$tempFolder\$($_.PackageFilename)"
-                    $unzipDir = "$tempFolder\$([IO.Path]::GetFileNameWithoutExtension($_.PackageFilename))"
-
-                    Write-Message "Unpacking file $($_.PackageFilename)..."
-                    7zip\Expand-Archive -Path $nupkgFile -OutputPath $unzipDir -Force -Silent -SuppressExtensionCheck
-
-                    $stagingDir = "$tempFolder\.staging\$modulesFolderTempName\$($_.Name)"
-
-                    $robocopyArgs = @{
-                        'Source' = $unzipDir
-                        'Destination' = $stagingDir
-                        'MirrorDirectoryTree' = $true
-                        'ExcludeFiles' = @('[Content_Types].xml', "$($_.Name).nuspec")
-                        'ExcludeDirectories' = @('package', '_rels')
+                    if (Test-Path "$tempFolder\.staging\$modulesFolderTempName") {
+                        Write-Verbose "Cleaning up old temp directory..."
+                        Remove-Item "$tempFolder\.staging\$modulesFolderTempName" -Force -Recurse -Confirm:$false | Out-Null
                     }
 
-                    Invoke-Robocopy @robocopyArgs | Out-Null
+                    mkdir "$tempFolder\.staging\$modulesFolderTempName" | Out-Null
 
-                    # Remove-Item $unzipDir -Force -Recurse -Confirm:$false | Out-Null
+                    $packagesToUnpack | ForEach-Object {
+                        $nupkgFile = "$tempFolder\$($_.PackageFilename)"
+                        $unzipDir = "$tempFolder\$([IO.Path]::GetFileNameWithoutExtension($_.PackageFilename))"
 
-                    $robocopyArgs = @{
-                        'Source' = $stagingDir
-                        'Destination' = "$($modulesFolder)\$($_.Name)"
-                        'MirrorDirectoryTree' = $true
+                        Write-Message "Unpacking file $($_.PackageFilename)..."
+                        7zip\Expand-Archive -Path $nupkgFile -OutputPath $unzipDir -Force -Silent -SuppressExtensionCheck
+
+                        $stagingDir = "$tempFolder\.staging\$modulesFolderTempName\$($_.Name)"
+
+                        $robocopyArgs = @{
+                            'Source' = $unzipDir
+                            'Destination' = $stagingDir
+                            'MirrorDirectoryTree' = $true
+                            'ExcludeFiles' = @('[Content_Types].xml', "$($_.Name).nuspec")
+                            'ExcludeDirectories' = @('package', '_rels')
+                        }
+
+                        Invoke-Robocopy @robocopyArgs | Out-Null
+
+                        # Remove-Item $unzipDir -Force -Recurse -Confirm:$false | Out-Null
+
+                        $robocopyArgs = @{
+                            'Source' = $stagingDir
+                            'Destination' = "$($modulesFolder)\$($_.Name)"
+                            'MirrorDirectoryTree' = $true
+                        }
+
+                        Write-Message "Copying files to .\Modules\$($_.Name)..."
+                        Invoke-Robocopy @robocopyArgs | Out-Null
                     }
-
-                    Write-Message "Copying files to .\Modules\$($_.Name)..."
-                    Invoke-Robocopy @robocopyArgs | Out-Null
                 }
+            } else {
+                Write-Error "Couldn't find package '$($Name)'."
             }
         } else {
-            Write-Error "Couldn't find package '$($Name)'."
+            Write-Error "Couldn't find module root."
         }
-    } else {
-        Write-Error "Couldn't find module root."
     }
 }
